9906cad61fcf0c2b2bdbf5719793ca58a6b5f490
Noxmor <lord.noxmor@gmail.com>
2024-04-29 03:10:53 +0200

    Refactored lazy.lua

--- .config/nvim/lua/noxmor/lazy.lua
+++ .config/nvim/lua/noxmor/lazy.lua
@@ -16,55 +16,20 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            require('nvim-treesitter.configs').setup({
-                ensure_installed = { "c", "lua", "vim" },
-                highlight = { enable = true }
-            })
-        end
-    },
-    "ellisonleao/gruvbox.nvim",
-    {
-        "kyazdani42/nvim-tree.lua",
-        config = function()
-            require('nvim-tree').setup({})
-        end
-    },
-    {
-        "windwp/nvim-autopairs",
-        config = function()
-            require('nvim-autopairs').setup({})
-        end
-    },
-    "tpope/vim-fugitive",
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v3.x"
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require('lsp-zero').extend_lspconfig()
-            require('lspconfig').clangd.setup({})
-            require('lspconfig').lua_ls.setup({})
-        end
-    },
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-nvim-lsp",
-    "L3MON4D3/LuaSnip",
-    {
-        "j-hui/fidget.nvim",
-        config = function()
-            require('fidget').setup({})
-        end
-    },
-    "nvim-lua/plenary.nvim",
-    "ThePrimeagen/vim-be-good",
-    "christoomey/vim-tmux-navigator"
-}, {})
+-- TODO: Add plugin files for these.
+--    "nvim-lua/plenary.nvim",
+--    "tpope/vim-fugitive",
+--    {
+--        "j-hui/fidget.nvim",
+--        config = function()
+--            require('fidget').setup({})
+--        end
+--    },
+--    "ThePrimeagen/vim-be-good",
+--    "christoomey/vim-tmux-navigator"
+
+require("lazy").setup({ { import = "noxmor.plugins" }, { import = "noxmor.plugins.lsp" } }, {
+    checker = {
+        enabled = true
+    }
+})
