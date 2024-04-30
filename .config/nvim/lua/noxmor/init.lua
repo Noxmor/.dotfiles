77fbdb209fc08ba5261ecc24d1de46100b39e110
Noxmor <lord.noxmor@gmail.com>
2024-04-30 22:11:03 +0200

    Removed obsolete keymaps

--- .config/nvim/lua/noxmor/init.lua
+++ .config/nvim/lua/noxmor/init.lua
@@ -4,13 +4,3 @@ require("noxmor.options")
 require("noxmor.keymaps")
 require("noxmor.lazy")
 
--- Better navigation for vim panes.
-vim.api.nvim_set_keymap('n', '<c-h>', ':wincmd h<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<c-j>', ':wincmd j<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<c-k>', ':wincmd k<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<c-l>', ':wincmd l<CR>', { noremap = true })
-
-vim.api.nvim_set_keymap('n', '<C-h>', ':TmuxNavigateLeft<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-j>', ':TmuxNavigateDown<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-k>', ':TmuxNavigateUp<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-l>', ':TmuxNavigateRight<CR>', { noremap = true })
