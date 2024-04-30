314b8255f1053e779ecdb96223f6b8de98a6eb08
Noxmor <lord.noxmor@gmail.com>
2024-04-30 22:10:45 +0200

    Refactored keymaps from init.lua

--- .config/nvim/lua/noxmor/keymaps.lua
+++ .config/nvim/lua/noxmor/keymaps.lua
@@ -7,3 +7,9 @@ local keymap = vim.keymap
 keymap.set('n', "<leader>nh", ":nohl<CR>", {
     desc = "Clear all search highlights."
 })
+
+-- Better navigation for vim panes.
+vim.api.nvim_set_keymap('n', "<C-h>", "<cmd>wincmd h<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', "<C-j>", "<cmd>wincmd j<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', "<C-k>", "<cmd>wincmd k<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', "<C-l>", "<cmd>wincmd l<CR>", { noremap = true })
