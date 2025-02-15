785048e9e2cd8c59109c35fcec5cc614f4c6e4e7
Noxmor <lord.noxmor@gmail.com>
2025-02-15 17:55:36 +0100

    Change nvim lualine theme to gruvbox_dark

--- nvim/.config/nvim/lua/noxmor/plugins/lualine.lua
+++ nvim/.config/nvim/lua/noxmor/plugins/lualine.lua
@@ -8,54 +8,11 @@ return {
     config = function()
         local lualine = require("lualine")
         local lazy_status = require("lazy.status")
-
-        local colors = {
-            blue = "#83A598",
-            green = "#B8BB26",
-            purple = "#D3869B",
-            yellow = "#FABD2F",
-            red = "#FB4934",
-            fg = "#c3ccdc",
-            bg = "#112638",
-            inactive_bg = "#2c3043"
-        }
-
-        local lualine_theme = {
-            normal = {
-                a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-                b = { bg = colors.bg, fg = colors.fg },
-                c = { bg = colors.bg, fg = colors.fg }
-            },
-            insert = {
-                a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-                b = { bg = colors.bg, fg = colors.fg },
-                c = { bg = colors.bg, fg = colors.fg }
-            },
-            visual = {
-                a = { bg = colors.purple, fg = colors.bg, gui = "bold" },
-                b = { bg = colors.bg, fg = colors.fg },
-                c = { bg = colors.bg, fg = colors.fg }
-            },
-            command = {
-                a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
-                b = { bg = colors.bg, fg = colors.fg },
-                c = { bg = colors.bg, fg = colors.fg }
-            },
-            replace = {
-                a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-                b = { bg = colors.bg, fg = colors.fg },
-                c = { bg = colors.bg, fg = colors.fg }
-            },
-            inactive = {
-                a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
-                b = { bg = colors.inactive_bg, fg = colors.semilightgray },
-                c = { bg = colors.inactive_bg, fg = colors.semilightgray }
-            }
-        }
+        local theme = require("lualine.themes.gruvbox_dark")
 
         lualine.setup({
             options = {
-                theme = lualine_theme
+                theme = theme
             },
             sections = {
                 lualine_x = {
