77782a06f8e1744a185eb23e8fc393b51c857ccd
Noxmor <lord.noxmor@gmail.com>
2025-04-06 16:03:28 +0200

    Replace git text with branch symbol and display prompt symbol based on git status

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,8 +12,15 @@ function fish_prompt
 
     set -l git_branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
 
+    set git_status (git status --porcelain 2>/dev/null)
+    if test -n "$git_status"
+        set git_symbol (set_color brred -b normal)" ✗ "
+    else
+        set git_symbol (set_color brgreen -b normal)" ✔ "
+    end
+
     if test -n "$git_branch"
-        echo -n (set_color cyan)""(set_color brwhite -b cyan)" $username "(set_color cyan -b blue)""(set_color brwhite -b blue)" $current_dir "(set_color blue -b brblack)""(set_color brwhite -b brblack)" git: $git_branch "(set_color brblack -b normal)""(set_color bryellow -b normal)" \$ "(set_color normal)
+        echo -n (set_color cyan)""(set_color brwhite -b cyan)" $username "(set_color cyan -b blue)""(set_color brwhite -b blue)" $current_dir "(set_color blue -b brblack)""(set_color brwhite -b brblack)"  $git_branch "(set_color brblack -b normal)"""$git_symbol"(set_color normal)
     else
         echo -n (set_color cyan)""(set_color brwhite -b cyan)" $username "(set_color cyan -b blue)""(set_color brwhite -b blue)" $current_dir "(set_color blue -b normal)""(set_color bryellow -b normal)" \$ "(set_color normal)
     end
