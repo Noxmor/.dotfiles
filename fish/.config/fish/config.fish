c7056eac36ab8c30de87cc25a24c121db2b3f5ad
Noxmor <lord.noxmor@gmail.com>
2025-04-06 02:04:03 +0200

    Add fish package

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,20 @@
+# Copyright (c) 2025 Noxmor
+
+set -g fish_greeting ""
+
+if status is-interactive
+    theme_gruvbox dark hard
+end
+
+function fish_prompt
+    set -l username (whoami)
+    set -l current_dir (pwd | string replace -r "^$HOME" "~")
+
+    set -l git_branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
+
+    if test -n "$git_branch"
+        echo -n (set_color cyan)""(set_color brwhite -b cyan)" $username "(set_color cyan -b blue)""(set_color brwhite -b blue)" $current_dir "(set_color blue -b brblack)""(set_color brwhite -b brblack)" git: $git_branch "(set_color brblack -b normal)""(set_color bryellow -b normal)" \$ "(set_color normal)
+    else
+        echo -n (set_color cyan)""(set_color brwhite -b cyan)" $username "(set_color cyan -b blue)""(set_color brwhite -b blue)" $current_dir "(set_color blue -b normal)""(set_color bryellow -b normal)" \$ "(set_color normal)
+    end
+end
