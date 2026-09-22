c09faa603faa2fb6ee4bd4580e2cf0a2bac3b21d
Noxmor <lord.noxmor@gmail.com>
2026-09-23 00:31:13 +0200

    fix(nvim): remove outdated 0.1.x branch pin for telescope.nvim

--- nvim/.config/nvim/lua/noxmor/plugins/telescope.lua
+++ nvim/.config/nvim/lua/noxmor/plugins/telescope.lua
@@ -2,7 +2,6 @@
 
 return {
     "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
         {
@@ -33,10 +32,13 @@ return {
 
         local keymap = vim.keymap
 
-        keymap.set('n', "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in the current working directory." })
+        keymap.set('n', "<leader>ff", "<cmd>Telescope find_files<CR>",
+            { desc = "Fuzzy find files in the current working directory." })
         keymap.set('n', "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files." })
-        keymap.set('n', "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Find string in current working directory." })
-        keymap.set('n', "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor in current working directory." })
+        keymap.set('n', "<leader>fs", "<cmd>Telescope live_grep<CR>",
+            { desc = "Find string in current working directory." })
+        keymap.set('n', "<leader>fc", "<cmd>Telescope grep_string<CR>",
+            { desc = "Find string under cursor in current working directory." })
         keymap.set('n', "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find todos." })
     end
 }
